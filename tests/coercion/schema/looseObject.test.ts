20728b43815316ae44fa1fca55a44978dfb4b02b
chimame <rito.tamata@gmail.com>
2024-11-21 01:06:45 +0900

    feat: Add support looseObject (#39)

--- tests/coercion/schema/looseObject.test.ts
+++ tests/coercion/schema/looseObject.test.ts
@@ -1,38 +1,32 @@
-import {
-  checkAsync,
-  forwardAsync,
-  number,
-  objectAsync,
-  pipeAsync,
-  string,
-} from "valibot";
+import { check, forward, looseObject, number, pipe, string } from "valibot";
 import { describe, expect, test } from "vitest";
 import { parseWithValibot } from "../../../parse";
 import { createFormData } from "../../helpers/FormData";
 
-describe("objectAsync", () => {
-  test("should pass only objects", async () => {
-    const schema1 = objectAsync({ key1: string(), key2: number() });
+describe("looseObject", () => {
+  test("should pass only loose objects", () => {
+    const schema1 = looseObject({ key1: string(), key2: number() });
     const input1 = createFormData("key1", "test");
     input1.append("key2", "123");
-    const output1 = await parseWithValibot(input1, { schema: schema1 });
+    const output1 = parseWithValibot(input1, { schema: schema1 });
     expect(output1).toMatchObject({
       status: "success",
       value: { key1: "test", key2: 123 },
     });
 
     input1.append("key3", "");
-    const output2 = await parseWithValibot(input1, { schema: schema1 });
+    const output2 = parseWithValibot(input1, { schema: schema1 });
     expect(output2.status).toBe("success");
     // @ts-expect-error
     expect(output2.value).toStrictEqual({
       key1: "test",
       key2: 123,
+      key3: "",
     });
 
     const input2 = createFormData("key1", "");
     input2.append("key2", "123");
-    const output3 = await parseWithValibot(input2, { schema: schema1 });
+    const output3 = parseWithValibot(input2, { schema: schema1 });
     expect(output3).toMatchObject({
       error: {
         key1: expect.anything(),
@@ -41,7 +35,7 @@ describe("objectAsync", () => {
 
     const input3 = createFormData("key1", "string");
     input3.set("key2", "non number");
-    const output4 = await parseWithValibot(input3, { schema: schema1 });
+    const output4 = parseWithValibot(input3, { schema: schema1 });
     expect(output4).toMatchObject({
       error: {
         key2: expect.anything(),
@@ -49,18 +43,18 @@ describe("objectAsync", () => {
     });
   });
 
-  test("should pass objects with pipe", async () => {
-    const schema = pipeAsync(
-      objectAsync({
+  test("should pass objects with pipe", () => {
+    const schema = pipe(
+      looseObject({
         key: string(),
       }),
-      forwardAsync(
-        checkAsync(async ({ key }) => key !== "error name", "key is error"),
+      forward(
+        check(({ key }) => key !== "error name", "key is error"),
         ["key"],
       ),
     );
 
-    const output = await parseWithValibot(createFormData("key", "valid"), {
+    const output = parseWithValibot(createFormData("key", "valid"), {
       schema,
     });
     expect(output).toMatchObject({
@@ -68,12 +62,9 @@ describe("objectAsync", () => {
       value: { key: "valid" },
     });
 
-    const errorOutput = await parseWithValibot(
-      createFormData("key", "error name"),
-      {
-        schema,
-      },
-    );
+    const errorOutput = parseWithValibot(createFormData("key", "error name"), {
+      schema,
+    });
     expect(errorOutput).toMatchObject({
       error: {
         key: expect.anything(),
